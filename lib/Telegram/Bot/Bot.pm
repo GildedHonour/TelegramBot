ff182f20ffa2153233905052b37bbce201d48d3a
Alex Maslakov <me@gildedhonour.com>
2015-09-30 18:45:21 +0800

    wip

--- lib/Telegram/Bot/Bot.pm
+++ lib/Telegram/Bot/Bot.pm
@@ -1,14 +1,27 @@
 use HTTP::UserAgent;
+use JSON::Tiny;
 
 class Telegram::Bot {
-  my has $.token;
-  my $http-client = HTTP::UserAgent.new;
+  has $.token;
+  has $http-client = HTTP::UserAgent.new;
 
   method get-updates($offset, $limit, $timeout)
   method set-webhook($url, $certificate)
   method get-me() {
     $full_url = build-url($token, "getMe");
-    $http-client
+    try my $response = !send-request("getMe", "some type3243");
+    if $response.is-success {
+      $user-json = from-json($response.content);
+      my $user = User.new(
+        id => user-json{'id'},
+        first-name => user-json{'first-name'},
+        last-name => user-json{'last-name'},
+        user-name => user-json{'user-name'}
+      );
+      return $user;
+    } else {
+      die $response.status-line;
+    }
   }
   method send-message($chat-id, $text, $parse-mode, $disable-web-page-preview, $reply-to-message-id, $reply-markup)
   method forward-message($chat-id, $from-chat-id, $message-id)
@@ -25,9 +38,11 @@ class Telegram::Bot {
   method get-user-profile-photos($user-id, $offset, $limit)
   method get-file($file-id)
 
-  method !send-request($url, $method) {
-    $http-client.get(build-url($token, $url));
+  method !send-request($method, $type) {
+    return $http-client.get(build-url($token, $method)); # or post  $type
   }
 
+ $ua.get($full_url);
+ 
   sub !build-url($token, $method-name) { "https://api.telegram.org/bot$token/$method-name" }
 }
